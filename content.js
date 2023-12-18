ff85d74d04e33c42cdfb5f38a46313b4ff41be33
Darshan Poudel <pranishpoudel10@gmail.com>
2023-12-18 17:27:23 +0545

    Chrome Extension made

---
+++ content.js
@@ -0,0 +1,10 @@
+const credentials = {
+  username: "079bct036",
+  password: "<<---YOUR PASSWORD GOES HERE---->>",
+};
+const login = () => {
+  username.value = credentials.username;
+  password.value = credentials.password;
+  loginbutton.click();
+};
+login();
