b03fb8ba8ddd643c3eac9889213a6e3c1488f4d9
Darshan Poudel <pranishpoudel10@gmail.com>
2023-12-19 07:58:25 +0545

    test

--- content.js
+++ content.js
@@ -2,9 +2,21 @@ const credentials = {
   username: "<<---YOUR USERNAME GOES HERE---->>",
   password: "<<---YOUR PASSWORD GOES HERE---->>",
 };
+
 const login = () => {
   username.value = credentials.username;
   password.value = credentials.password;
+
   loginbutton.click();
 };
+
+function saveAndRet(my_credentials) {
+  chrome.storage.sync.set(my_credentials, function () {
+    console.log("Data saved in content.js:", my_credentials);
+  });
+
+  chrome.storage.sync.get(["my_username", "my_password"], function (result) {
+    console.log("Data retrieved in content.js:", result);
+  });
+}
 login();
